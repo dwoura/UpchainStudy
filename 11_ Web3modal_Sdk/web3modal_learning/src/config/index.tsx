11c9563bc56c41e6f1f4c05b96b639957af0f58a
dwoura <568722524@qq.com>
2024-10-22 21:22:45 +0800

    fix: package

--- 11_ Web3modal_Sdk/web3modal_learning/src/config/index.tsx
+++ 11_ Web3modal_Sdk/web3modal_learning/src/config/index.tsx
@@ -3,7 +3,7 @@
 
 import { cookieStorage, createStorage } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { mainnet, arbitrum } from '@reown/appkit/networks'
+import { mainnet, sepolia } from '@reown/appkit/networks'
 
 // Get projectId from https://cloud.reown.com
 export const projectId = '0925f809245f004812c057b040367ff1'
@@ -12,7 +12,7 @@ if (!projectId) {
   throw new Error('Project ID is not defined')
 }
 
-export const networks = [mainnet, arbitrum]
+export const networks = [mainnet, sepolia]
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
