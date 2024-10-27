52856417888d885de39a9f045064224c1b3888d0
dwoura <568722524@qq.com>
2024-10-27 13:43:29 +0800

    feat: fix getEvents

--- 09_Viem_Wagmi/learning/getEventsByContractWithABI.ts
+++ 09_Viem_Wagmi/learning/getEventsByContractWithABI.ts
@@ -1,11 +1,13 @@
 // 查询Ethereum链上最近100个区块链内的 USDC Transfer记录
 // USDC: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
+// 关键：使用contract.getEvents 函数，需要使用合约 ABI，因为contract 对象需要 ABI
 
-import { createPublicClient,getContract, createWalletClient, http } from "viem";
+// import * as viem from "viem"; // 方便使用但可读性较差
+import { createPublicClient,getContract, http, formatUnits } from "viem";
 import { mainnet } from "viem/chains";
 import dotenv from "dotenv";
 import { usdCimplConfig } from "../src/generated"; // 使用相对路径前设置好 tsconfig 中的 baseUrl
-import { Address } from 'viem'; // 为了解决强类型地址类型
+import { Address } from 'viem'; // 强类型地址类型
 
 dotenv.config();
 
@@ -14,6 +16,7 @@ const client = createPublicClient({
   transport: http(process.env.ETH_RPC_URL),
 });
 
+// usdc 合约对象
 const contract = getContract({
     address: usdCimplConfig.address[2], // 存储合约的地址
     abi: usdCimplConfig.abi,    // 逻辑合约的 abi
@@ -22,6 +25,8 @@ const contract = getContract({
 
 async function getUSDCEvents(){
     const latestBlock:bigint = await client.getBlockNumber();
+    const decimals = await contract.read.decimals();
+
     const logs = await contract.getEvents.Transfer(
         {
         },
@@ -37,7 +42,7 @@ async function getUSDCEvents(){
             log.args.from?.toString(),
             "转账给",
             log.args.to?.toString(),
-            log.args.value?.toString(),
+            formatUnits(log.args.value as bigint, decimals ),
             "USDC, 交易ID：",
             log.transactionHash.toString()
         );
@@ -74,30 +79,11 @@ async function getUSDCEvents(){
 //     ); 
 // }
 
-//从 xx 转账给 xx 99.12345 USDC ,交易ID：
 async function main() {
-
-    // console.log('Transfer logs:', logs);
-    // const unwatch = contract.watchEvent.Transfer(
-    //     {
-    //         from: '0x0920D73e552605a81BD5434Ca25d3743EAf199a5',
-    //         to: '0x0920D73e552605a81BD5434Ca25d3743EAf199a5'
-    //     },
-    //     { 
-    //         fromBlock: BigInt(21050366), // 确保这个区块范围包含您要监听的事件
-    //         onLogs: logs => {
-    //             if (logs.length > 0) {
-    //                 console.log('Transfer logs:', logs);
-    //             } else {
-    //                 console.log('No Transfer events found in the specified block range.');
-    //             }
-    //         },
-    //         onError: error => console.error('Error:', error)
-    //     }
-    // );  
-
     //getLatest100EventsOfUSDC();
+
     getUSDCEvents();
+    
 }
 
 main();
---
+++ 09_Viem_Wagmi/learning/getEventsByParseAbi.ts
@@ -0,0 +1,50 @@
+// 查询Ethereum链上最近100个区块链内的 USDC Transfer记录
+// USDC: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
+// 关键：使用client.createEventFilter 函数，无需使用合约ABI。但需要使用 parseAbiItem 来 parse 单条ABI
+
+import { createPublicClient, http,parseAbiItem ,formatUnits, decodeAbiParameters} from "viem";
+import { mainnet } from "viem/chains";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const client = createPublicClient({
+  chain: mainnet,
+  transport: http(process.env.ETH_RPC_URL),
+});
+
+async function getUSDCEventsWithFilter(){
+    const latestBlock:bigint = await client.getBlockNumber();
+
+    // 创建一个 Transfer 事件的 filter
+    const filter = await client.createEventFilter(
+        {
+            address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB4', // USDC address
+            event: parseAbiItem('event Transfer(address indexed from, address indexed to, uint256 value)'), // 单条transfer事件的ABI
+            fromBlock: latestBlock - BigInt(100), 
+            toBlock: latestBlock
+        }
+    )
+
+    // 获取 filter 所匹配的 logs 并打印出来
+    const logs = await client.getFilterLogs({filter});
+    console.log("打印 log")
+    logs.forEach(log => {
+        console.log(
+            "从",
+            log.args.from,
+            "转账给",
+            log.args.to,
+            formatUnits(log.args.value as bigint, 6),
+            "USDC, 交易ID：",
+            log.transactionHash
+        );
+    });
+}
+
+async function main() {
+    getUSDCEventsWithFilter();
+}
+
+main();
